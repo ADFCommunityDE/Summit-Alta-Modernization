b5093ca6c642b38ab7fb3558d9c470a574a4bf59
Markus Klenke <mke@team-pb.de>
2015-11-13 15:28:51 +0100

    disabled customer navigaton for all cases except for summit management

--- SummitAltafy/ViewController/src/de/adfcommunity/altaui/view/PageBean.java
+++ SummitAltafy/ViewController/src/de/adfcommunity/altaui/view/PageBean.java
@@ -1,8 +1,13 @@
 package de.adfcommunity.altaui.view;
 
+import de.adfcommunity.altaui.view.utils.JSFUtils;
+
 import java.io.Serializable;
 
+import javax.faces.component.UIComponent;
+
 import oracle.adf.controller.TaskFlowId;
+import oracle.adf.view.rich.context.AdfFacesContext;
 
 public class PageBean
     implements Serializable {
@@ -21,16 +26,31 @@ public class PageBean
 
     public String dashboard() {
         setDynamicTaskFlowId("/WEB-INF/dashboard.xml#dashboard");
+        triggerDeck();
         return null;
     }
 
     public String summitmanagement() {
         setDynamicTaskFlowId("/WEB-INF/summit-management.xml#summit-management");
+        triggerDeck();
         return null;
     }
 
     public String inventorycontrol() {
         setDynamicTaskFlowId("/WEB-INF/inventory-control.xml#inventory-control");
+        triggerDeck();
         return null;
+    }
+
+    public String getTaskFlowId() {
+        return taskFlowId;
+    }
+    
+    private void triggerDeck(){
+        // #{CustomerNavigation.deck}
+        UIComponent deck = (UIComponent)JSFUtils.resolveExpression("#{CustomerNavigation.deck}");
+        
+        AdfFacesContext.getCurrentInstance().addPartialTarget(deck);
+        
     }
 }
